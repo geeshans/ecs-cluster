89d8dcf30c4b42472c54d1217089282507115d93
Geeshan Siriwardena <geeshan@codegen.net>
2018-07-15 23:37:54 +0530

    Configuring the compute resources

--- main.tf
+++ main.tf
@@ -38,3 +38,80 @@ resource "aws_route_table_association" "a" {
   subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
   route_table_id = "${aws_route_table.r.id}"
 }
+
+### Compute
+
+resource "aws_autoscaling_group" "app" {
+  name                 = "app-asg"
+  vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
+  min_size             = "${var.asg_min}"
+  max_size             = "${var.asg_max}"
+  desired_capacity     = "${var.asg_desired}"
+  launch_configuration = "${aws_launch_configuration.app.name}"
+}
+
+data "template_file" "container_instance_base_cloud_config" {
+  template = "${file("${path.module}/cloud-config/base-container-instance.yml.tpl")}"
+
+  vars {
+    ecs_cluster_name = "${aws_ecs_cluster.container_instance.name}"
+  }
+}
+
+data "template_cloudinit_config" "container_instance_cloud_config" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.container_instance_base_cloud_config.rendered}"
+  }
+
+  part {
+    content_type = "${var.cloud_config_content_type}"
+    content      = "${var.cloud_config_content}"
+  }
+}
+
+data "aws_ami" "ecs_optimized_ami" {
+  most_recent = true
+
+   filter {
+    name   = "name"
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+  filter {
+    name = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+resource "aws_launch_configuration" "app" {
+  security_groups = [
+    "${aws_security_group.instance_sg.id}",
+  ]
+
+  key_name                    = "${var.key_name}"
+  image_id                    = "${data.aws_ami.ecs_optimized_ami.id}"
+  instance_type               = "${var.instance_type}"
+  iam_instance_profile        = "${aws_iam_instance_profile.app.name}"
+  user_data                   = "${data.template_cloudinit_config.container_instance_cloud_config.rendered}"
+
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
